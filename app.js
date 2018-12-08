2b1298112b37d05dd8a659ffd3e2e097faf77e86
棚原　嘉 <ysm.t02@gmail.com>
2018-12-08 18:23:09 +0900

    swiperの導入、それに伴いapp.jsファイルの作成

---
+++ app.js
@@ -0,0 +1,11 @@
+$(function(){
+  let mySwiper = new Swiper('.swiper-container',{
+  direction: '',
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: true
+  },
+  effect: 'fade',
+  })
+});
